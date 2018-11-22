ef8d9405acb4aaee7cc309b878b4957b0619c7ab
tminlun <1272443075@qq.com>
2018-11-22 13:37:37 +0800

    注册

--- forum/apps/operation/views.py
+++ forum/apps/operation/views.py
@@ -16,21 +16,22 @@ class LoginView(View):
         if login_form.is_valid():#是否规范
             username = request.POST.get('username')
             password = request.POST.get('password')
-            user = auth.authenticate(username=username,password=password)#数据库是否有此用户
+            user = auth.authenticate(username=username, password=password)#和数据库账号密码是否相同
             if user is not None:#有此用户
                 auth.login(request, user)
                 return redirect(reverse('index'))#登录成功重定向到 首页
             else:
                 return render(request, 'login_or_registered.html',{
-                    'errors':'没有此用户',
+                    'errors': '账号或者密码错误',
                     'login_form': login_form
                 })
         else:
-            return render(request, 'login_or_registered.html', {
-                'errors': '账号或者密码错误',
-                'login_form': login_form
-            })
+            return render(request, 'login_or_registered.html',{
+                    'errors': '输入错误',
+                    'login_form': login_form
+                })
 
+#注册
 class RegisteredView(View):
     def get(self, request):
         registered_form = RegisteredForm()
